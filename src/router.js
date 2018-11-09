6e599eaa566e6fae39cf9e0f85638e0effbe96ef
Logan Mace <lmmace92@hotmail.com>
2018-11-09 15:20:52 -0600

    changed names

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Guitars from './views/Guitars.vue'
 
 Vue.use(Router)
 
@@ -10,16 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'guitars',
+      component: Guitars
     },
     {
-      path: '/mens',
-      name: 'mens',
+      path: '/bass',
+      name: 'bass',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Mens.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Bass.vue')
     },
     {
       path: '/product/:id',
