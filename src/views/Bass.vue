bfb5a7e0705a13940fbb3088829e49258bcbfe04
Logan Mace <lmmace92@hotmail.com>
2018-11-14 13:52:58 -0600

    fix

---
+++ src/views/Bass.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="products-list">
+    <Product 
+      v-for="product in productsList" 
+      :name="product.name" 
+      :price="product.price"
+      :id="product.id" 
+      :key="product.id"/>
+  </div>
+</template>
+
+<script>
+  import Product from '../components/Product.vue';
+
+  import {productsList} from '../products.js';
+
+  export default {
+    props: [],
+    components: {
+      Product
+    },
+    data() {
+      return {
+        productsList
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .products-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
