7599b81cb109c8e71c7cdebca96d8c064d56a0bd
Logan Mace <lmmace92@hotmail.com>
2018-11-15 14:52:17 -0600

    nav dropdowns added

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,98 @@
 <template>
-  <nav>
-    <ul>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Guitars">Guitars</router-link> |
-      <router-link to="/Bass">Bass</router-link> |
-      <router-link to="/Drums">Drums</router-link> |
-      <router-link to="/Keyboards">Keyboards</router-link> |
-      <router-link to="/Accessories">Accessories</router-link>
-    </ul>
-    <div>CART</div>
-  </nav>
+  <v-toolbar>
+    <v-toolbar-items>
+      <v-btn flat><router-link to="/">Home</router-link></v-btn>
+      <v-menu open-on-hover bottom auto offset-y attach>
+        <v-btn flat slot="activator">
+          <router-link to="/Guitars">Guitars</router-link>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Electric Guitars</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Acoustic Guitars</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-menu open-on-hover bottom auto offset-y attach>
+        <v-btn flat slot="activator">
+          <router-link to="/Bass">Bass</router-link>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Electric Basses</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Acoustic Basses</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-menu open-on-hover bottom auto offset-y attach>
+        <v-btn flat slot="activator">
+          <router-link to="/Drums">Drums</router-link>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Electric Drums</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Acoustic Drums</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-menu open-on-hover bottom auto offset-y attach>
+        <v-btn flat slot="activator">
+          <router-link to="/Keyboards">Keyboards</router-link>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Digital Pianos</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Synthesizers</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-menu open-on-hover bottom auto offset-y attach>
+        <v-btn flat slot="activator">
+          <router-link to="/Accessories">Accessories</router-link>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Strings</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn flat>
+              <v-list-tile-title>Cables</v-list-tile-title>
+            </v-btn>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>shopping_cart</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
 
 <script>
@@ -19,20 +102,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  nav {
-    padding: 20px;
-    background-color: #e4e4e4;
-    display: flex;
-    justify-content: space-between;
-    ul {
-      
-      a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-          color: #42a7b9;
-        }
-      }
-    }
+  a {
+    text-decoration: none;
+    color: black;
   }
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
+import colors from 'vuetify/es5/util/colors';
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    primary: '#2aabe2',
+    accent: colors.grey.darken3,
+    secondary: colors.amber.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3
+  }
+})
