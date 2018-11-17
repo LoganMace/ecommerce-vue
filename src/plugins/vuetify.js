09615352d4423240cf56c26b49b887ebd8952bfe
Logan Mace <lmmace92@hotmail.com>
2018-11-17 15:32:36 -0600

    fixed colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,12 +6,12 @@ import colors from 'vuetify/es5/util/colors';
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
-    primary: '#2aabe2',
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    primary: '#00796b',
+    accent: '#48a999',
+    secondary: colors.blue-grey.darken1,
+    // info: colors.teal.lighten1,
+    // warning: colors.amber.base,
+    error: colors.red.accent4,
+    // success: colors.green.accent3
   }
 })
