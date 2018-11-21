2e14e980bff0774e8aa3f1f84dbce46101670e13
Logan Mace <lmmace92@hotmail.com>
2018-11-21 16:48:24 -0600

    make route and component dynamic

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Guitars from './views/Guitars.vue'
 
 Vue.use(Router)
 
@@ -8,18 +7,23 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/Guitar',
+    //   name: 'Guitar',
+    //   component: Guitars
+    // },
+    // {
+    //   path: '/Bass',
+    //   name: 'Bass',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/Bass.vue')
+    // },
     {
-      path: '/Guitars',
-      name: 'Guitars',
-      component: Guitars
-    },
-    {
-      path: '/Bass',
-      name: 'Bass',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Bass.vue')
+      path: '/:category',
+      name: 'Category',
+      component: () => console.log('hit') || import('./views/Category')
     },
     {
       path: '/product/:id',
