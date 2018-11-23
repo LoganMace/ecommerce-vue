6459e77dc2c9ece78e092c8aa9a90ebd583a6139
Logan Mace <lmmace92@hotmail.com>
2018-11-23 10:25:46 -0600

    added specific category

--- src/router.js
+++ src/router.js
@@ -27,7 +27,7 @@ export default new Router({
     },
     {
       path: '/product/:id',
-      name: 'product',
+      name: 'Product',
       component: () => import('./components/ProdDetail.vue')
     }
   ]
---
+++ src/views/Category.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="category">
+    <Product 
+      v-for="product in products" 
+      :name="product.name" 
+      :price="product.price"
+      :images="product.images"
+      :id="product.id" 
+      :key="product.id"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+import Product from '../components/Product.vue';
+
+export default {
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProducts() {
+      axios.get(`http://localhost:3001/api/products/${this.$route.params.category}`).then(response => {
+        console.log(response.data);
+        this.products = response.data;
+      })
+    }
+  },
+  mounted() {
+    // this.$store.dispatch("getAllProducts")
+    this.getProducts();
+  },
+  watch: {
+    '$route': 'getProducts'
+  },
+  components: {
+    Product
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .category {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 5px;
+  }
+</style>
