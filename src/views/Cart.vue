4f02f5994166f30501b682e312b912fdcdb99025
Logan Mace <lmmace92@hotmail.com>
2018-11-29 11:25:23 -0600

    styled cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,16 +1,35 @@
 <template>
-  <div>
-    <div v-for="item in cart" :key="item.id">
-      <p>{{ item.name }}</p>
-      <p>{{ item.price }}</p>
-      <p>{{ item.quantity }}</p>
-    </div>
-  </div>
+<div>
+  <v-data-table
+    :headers="headers"
+    :items="cart"
+    :hide-actions="true"
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td class="text-xs-left">{{ props.item.name }}</td>
+      <td class="text-xs-left">{{ Number(props.item.price).toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</td>
+      <td class="text-xs-left">{{ props.item.quantity }}</td>
+      <td class="text-xs-left">{{ Number(props.item.price * props.item.quantity).toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</td>
+    </template>
+  </v-data-table>
+  <h1>Total: {{ cart.reduce((total, curr) => total + (curr.price * curr.quantity), 0).toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</h1>
+</div>
 </template>
 
 <script>
   import {mapGetters} from 'vuex'
   export default {
+    data() {
+      return {
+        headers: [
+          {text: 'Name', value: 'name', sortable: false},
+          {text: 'Price', value: 'price', sortable: false},
+          {text: 'Quantity', value: 'quantity', sortable: false},
+          {text: 'Total', value: 'total', sortable: false}
+        ]
+      }
+    },
     computed: {
       ...mapGetters({
         cart: 'getCart'
@@ -20,5 +39,8 @@
 </script>
 
 <style lang="scss" scoped>
-
+  h1 {
+    text-align: right;
+    margin: 5px 10px;
+  }
 </style>
